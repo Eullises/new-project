9b152f61300b92d50e378996139ba83e07b3f973
Eullises <eulises@hotmail.cl>
2023-03-09 14:19:18 -0300

    first commit, base project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './public/contact/contact.component';
+import { HomeComponent } from './public/home/home.component';
+import { ProductComponent } from './public/product/product.component';
+import { SupportComponent } from './public/support/support.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'home', pathMatch:'full'},
+  { path:'home', component:HomeComponent},
+  { path:'product', component:ProductComponent},
+  { path:'support', component:SupportComponent},
+  { path:'contact', component: ContactComponent},
+  { path:'**', redirectTo:'home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public/home/home.component';
+import { ProductComponent } from './public/product/product.component';
+import { SupportComponent } from './public/support/support.component';
+import { ContactComponent } from './public/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    SupportComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
